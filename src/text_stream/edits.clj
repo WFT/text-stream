2bfae9bfdaa37ee133e64ffbc083abd704630b8a
WFT <will.a.ft@gmail.com>
2015-05-28 17:03:28 -0500

    functions for handling commands as strings

--- src/text_stream/edits.clj
+++ src/text_stream/edits.clj
@@ -1,18 +1,18 @@
 (ns text-stream.edits
   (:gen-class))
 
-(defn perform
-  "Performs the `edits` (in the manner defined by the `consolidate` function)
-  on the provided `source-map`."
-  [edits source-map]
-  ((consolidate edits) source-map))
-
 (defn consolidate
   "Takes a seq of edits and composes them into a single function. Applies from
   last to first of edits seq. This makes composing a list of edits very easy
   with `cons`."
   [edits]
   (apply comp edits))
+
+(defn perform
+  "Performs the `edits` (in the manner defined by the `consolidate` function)
+  on the provided `source-map`."
+  [edits source-map]
+  ((consolidate edits) source-map))
 
 (defn cursor
   "Produces an edit function which moves the cursor to index `position-index`
@@ -26,7 +26,6 @@
   the current cursor position."
   [text-insertion]
   (fn [source-map]
-    (println "insert into:" source-map)
     (let [old-text (:text source-map)
           cursor (:pos source-map)
           new-text (str
@@ -41,7 +40,6 @@
   the text from the current cursor position (backwards)."
   [deletion-count]
   (fn [source-map]
-    (println "delete from" source-map)
     (let [old-text (:text source-map)
           cursor (:pos source-map)
           new-position (- cursor deletion-count)
@@ -60,5 +58,49 @@
 
 (defn initial-text
   [text]
-  ((insert-text text)
+  ((insert text)
    {:pos 0 :text ""}))
+
+(def commands
+  "Note: all commands happen to be 6 characters. 
+  We'll exploit this later."
+  {"inited" initial-text
+   "insert" insert
+   "delete" delete
+   "cursor" cursor})
+
+(def cmd-len "Length of commands." 6)
+
+(def cmd-validators
+  "Functions which convert a string to fit type expected by command,
+  or, if not possible, return `nil`."
+  (let [parse-int #(try (Integer/parseInt %)
+                       (catch NumberFormatException _ nil))]
+    {;; inited requires a different function, as it is only valid as the
+     ;; first message in a stream
+     initial-text (fn [_ _] nil)
+     insert (fn [in _] (and (string? in) in))
+     delete (fn [in source-map]
+              (if-let [n (parse-int in)]
+                (and (<= (:pos source-map) n) n)))
+     cursor (fn [in source-map]
+              (if-let [n (parse-int in)]
+                (and (<= (count (:text source-map))) n)))}))
+
+(defn valid-command?
+  "Is this a valid command? Returns the command string."
+  [text]
+  (and (>= (count text) (inc cmd-len))
+       (let [cmd (subs text 0 cmd-len)]
+         (and
+          (contains? commands cmd)
+          cmd))))
+
+(defn process-text-command
+  [text source-map]
+  (if-let [cmd (valid-command? text)]
+    (let [func (get commands cmd)
+          text-input (subs text cmd-len)]
+      (if-let [valid-input ((get cmd-validators func)
+                            text-input source-map)]
+        ((func valid-input) source-map)))))
