e48b1e199561681714fa6a36136ffeaf08ba3873
Will Field-Thompson <will.a.ft@gmail.com>
2015-06-18 13:06:19 -0400

    fixing some db connection issues on heroku

--- src/text_stream/data.clj
+++ src/text_stream/data.clj
@@ -3,8 +3,22 @@
             [korma.core :refer :all]
             [text-stream.edits :as edits]))
 
-(defdb db (postgres {:db "textstream"
-                  :user "field-thompson"}))
+
+
+(defdb db
+  (if (System/getenv "DATABASE_URL")
+    (let [db-uri (java.net.URI. (System/getenv "DATABASE_URL"))
+          user-and-password (clojure.string/split (.getUserInfo db-uri) #":")]
+      {:classname "org.postgresql.Driver"
+       :subprotocol "postgresql"
+       :user (get user-and-password 0)
+       :password (get user-and-password 1) ; may be nil
+       :subname (if (= -1 (.getPort db-uri))
+                  (format "//%s%s" (.getHost db-uri) (.getPath db-uri))
+                  (format "//%s:%s%s" (.getHost db-uri) (.getPort db-uri)
+                          (.getPath db-uri)))})
+    (postgres
+     {:db "textstream"})))
 
 (defentity stream)
 
